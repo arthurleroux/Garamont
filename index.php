b8b6cbe187be75cd36b6f14d68ede836bfab9b15
Arthur Leroux <Arthur@MacBook-Pro-de-Arthur-2.local>
2016-04-06 15:59:48 +0200

    slider fonctionnel

--- index.php
+++ index.php
@@ -1,40 +1,73 @@
 <?php require("header.php"); ?>
 
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <!--
-    <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    </ol>
-    -->
+<div class="home">
+    <div class="container-fluid">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <!--
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            </ol>
+            -->
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-        <div class="item active">
-            <img src="img/eleves.jpg" alt="...">
-            <div class="carousel-caption">
-
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="img/eleves.jpg" alt="...">
+                    <div class="mask">
+                    </div>
+                    <div class="carousel-caption">
+                        JHVLY
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="img/eleves.jpg" alt="...">
+                    <div class="mask">
+                    </div>
+                    <div class="carousel-caption">
+                        JHVLY
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="item">
-            <img src="img/eleves.jpg" alt="...">
-            <div class="carousel-caption">
 
-            </div>
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
-
-    <!-- Controls -->
-    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
 </div>
+
+<div class="container">
+    <div class="row">
+        <div class="text-center">
+            <h1>Formations</h1>
+        </div>
+    </div>
+</div>
+
+<!--<div class="part">
+    <h2>Losange</h2>
+    <div class="losange">
+        <div class="los1">
+            <img src="http://farm3.staticflickr.com/2178/3531465579_8bff044e9b_z.jpg?zz=1" alt="" width="255" height="320" />
+        </div>
+    </div>
+</div>
+<div class="part">
+    <h2>Losange</h2>
+    <div class="losange">
+        <div class="los1">
+            <img src="http://farm3.staticflickr.com/2178/3531465579_8bff044e9b_z.jpg?zz=1" alt="" width="255" height="320" />
+        </div>
+    </div>
+</div>-->
 
 <?php require("footer.php"); ?>
