23b84f9e0e6485138796a2f80d53ebb8cba5a79e
Arthur Leroux <Arthur@MacBook-Pro-de-Arthur-2.local>
2016-04-08 08:36:41 +0200

    résolution bug des flèches sur le slider sous chrome

--- index.php
+++ index.php
@@ -1,54 +1,54 @@
 <?php require("header.php"); ?>
 
-<div class="home">
-    <div class="container-fluid">
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
+<div class="parallax-window" data-parallax="scroll" data-image-src="img/garamont_background.jpg">
+    <div class="home">
+        <div class="container-fluid">
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
 
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            </ol>
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
 
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img src="img/eleves.jpg" alt="...">
-                    <div class="mask">
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="img/eleves.jpg" alt="...">
+                        <div class="mask">
+                        </div>
+                        <div class="carousel-caption">
+                            <img src="img/logo_garamont.png" alt="">
+                        </div>
                     </div>
-                    <div class="carousel-caption">
-                        <img src="img/logo_garamont.png" alt="">
+                    <div class="item">
+                        <img src="img/eleves.jpg" alt="...">
+                        <div class="mask">
+                        </div>
+                        <div class="carousel-caption">
+                            <img src="img/logo_garamont.png" alt="">
+                        </div>
                     </div>
                 </div>
-                <div class="item">
-                    <img src="img/eleves.jpg" alt="...">
-                    <div class="mask">
-                    </div>
-                    <div class="carousel-caption">
-                        <img src="img/logo_garamont.png" alt="">
-                    </div>
-                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <img src="img/FlecheGauche.png" alt="">
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <img src="img/FlecheDroite.png" alt="">
+                </a>
             </div>
-
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                <img src="img/FlecheGauche.png" alt="">
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                <img src="img/FlecheDroite.png" alt="">
-            </a>
         </div>
     </div>
-</div>
 
 
-<!---------------------Formation---------------------->
+    <!---------------------Formation---------------------->
 
 
 
-<div class="zga parallax-window" data-parallax="scroll" data-image-src="img/garamont_background.jpg">
     <div class="container-fluid block-formations">
         <div class="row">
             <div class="text-center">
@@ -84,6 +84,13 @@
         </div>
     </div>
 
+    <div class="container-fluid actus">
+        <div class="row">
+            <div class="text-center">
+                <h1>Actualités</h1>
+            </div>
+        </div>
+    </div>
 
     <!---------------------Galerie---------------------->
 
@@ -137,6 +144,7 @@
 
     </div>
 </div>
+
 
 
 
