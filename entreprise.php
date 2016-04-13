e3d6c9099dadbcb4d2b45514bc0c300af2ed70a8
Elies Makhlouf <elies.makhlouf@gmail.comb@domain.ltd>
2016-04-13 17:05:20 +0200

    visuel entreprise

--- entreprise.php
+++ entreprise.php
@@ -1,13 +1,12 @@
 <?php  require ('header.php'); ?>
 
-<div class="visuel-entreprise">
-    <div class="container-fluid"></div>
-</div>
-
-<div class="parallax-window" data-parallax="scroll" data-image-src="img/garamont_background.jpg">
-    <div class="container entreprise">
-        <h1>Entreprise</h1>
+    <div class="parallax-window" data-parallax="scroll" data-image-src="img/garamont_background.jpg">
+        <div class="visuel-entreprise">
+            <div class="container-fluid"></div>
+        </div>
+        <div class="container entreprise">
+            <h1>Entreprise</h1>
+        </div>
     </div>
-</div>
 
 <?php require ('footer.php'); ?>
