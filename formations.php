f1cbb58773e9e079b36461ffded56ab93c91dfab
Elies Makhlouf <elies.makhlouf@gmail.comb@domain.ltd>
2016-04-13 18:01:55 +0200

    Projets, Formations : image + titre

--- formations.php
+++ formations.php
@@ -1,5 +1,14 @@
 <?php require("header.php"); ?>
 
+    <div class="visuel-formation">
+        <div class="container-fluid img-section">
+            <div class="mask">
+            </div>
+        </div>
+    </div>
 
+    <div class="container text-center">
+        <h1>Formations</h1>
+    </div>
 
 <?php require("footer.php"); ?>
--- projets.php
+++ projets.php
@@ -1,5 +1,13 @@
 <?php require("header.php"); ?>
 
+    <div class="visuel-projets">
+        <div class="container-fluid img-section">
+            <div class="mask"></div>
+        </div>
+    </div>
 
+    <div class="container text-center">
+        <h1>Projets</h1>
+    </div>
 
 <?php require("footer.php"); ?>
