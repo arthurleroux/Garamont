5f0db87b13c8cb48ce80bf2a2deb78fe3c359412
Elies Makhlouf <elies.makhlouf@gmail.comb@domain.ltd>
2016-04-13 13:47:40 +0200

    Test 2 : le less ne génère pas de code sur le fichier css

--- entreprise.php
+++ entreprise.php
@@ -3,4 +3,10 @@
 <header></header>
 
 
+<div class="parallax-window" data-parallax="scroll" data-image-src="img/garamont_background.jpg">
+    <div class="container entreprise">
+        <h1>Entreprise</h1>
+    </div>
+</div>
+
 <?php include ('footer.php'); ?>
