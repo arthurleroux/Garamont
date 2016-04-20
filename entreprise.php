320e42d281163722cef5b46d3cab9749bf2040a9
Elies Makhlouf <elies.makhlouf@gmail.comb@domain.ltd>
2016-04-20 17:24:36 +0200

    page entreprise

--- entreprise.php
+++ entreprise.php
@@ -4,8 +4,50 @@
         <div class="visuel-entreprise">
             <div class="container-fluid"></div>
         </div>
-        <div class="container entreprise">
-            <h1>Entreprise</h1>
+        <div class="container-fluid entreprise">
+
+            <div class="title-entreprise text-center">
+                <h1>Entreprise</h1>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="taxe">
+                            <h2>Taxe d'apprentissage</h2>
+                            <h3>Les rendez-vous formation & compétences</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque aut dignissimos ea libero, magnam nemo placeat repellendus ut voluptatibus. Animi corporis distinctio et ipsum vero. Magnam quae sed sint.</p>
+                            <div class="col-md-offset-2">
+                                <p><strong>Horaire</strong></p>
+                                <p><strong>Lieu</strong></p>
+                                <p><strong>Information</strong></p>
+                            </div>
+                            <a href="">Se renseigner et choisir</a>
+                        </div>
+                        <div class="bourses">
+                            <h2>Taxe d'apprentissage</h2>
+                            <h3>Les rendez-vous formation & compétences</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque aut dignissimos ea libero, magnam nemo placeat repellendus ut voluptatibus. Animi corporis distinctio et ipsum vero. Magnam quae sed sint.</p>
+                            <p><strong>Horaire</strong></p>
+                            <p><strong>Lieu</strong></p>
+                            <p><strong>Information</strong></p>
+                            <div class="button-bourses text-right">
+                                <a href="" class="btn btn-info">En savoir +</a>
+                            </div>
+                        </div>
+                        <div class="marches">
+                            <h2>Taxe d'apprentissage</h2>
+                            <h3>Les rendez-vous formation & compétences</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque aut dignissimos ea libero, magnam nemo placeat repellendus ut voluptatibus. Animi corporis distinctio et ipsum vero. Magnam quae sed sint.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="offres">
+                            <h2>Offres d'emplois</h2>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
