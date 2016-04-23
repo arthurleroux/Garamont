f08f5bb05e6913b6ecfe9e6d6af4c822b2e6efe8
Elies Makhlouf <elies.makhlouf@gmail.comb@domain.ltd>
2016-04-23 13:34:21 +0200

    Fin de l'intégration de la page entreprise

--- entreprise.php
+++ entreprise.php
@@ -5,45 +5,89 @@
             <div class="container-fluid"></div>
         </div>
         <div class="container-fluid entreprise">
-
             <div class="title-entreprise text-center">
                 <h1>Entreprise</h1>
             </div>
-            <div class="container">
+            <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-8 contenu-entreprise-1">
                         <div class="taxe">
                             <h2>Taxe d'apprentissage</h2>
                             <h3>Les rendez-vous formation & compétences</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque aut dignissimos ea libero, magnam nemo placeat repellendus ut voluptatibus. Animi corporis distinctio et ipsum vero. Magnam quae sed sint. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores earum eius et fugit molestiae, necessitatibus nulla obcaecati officia placeat quae quas quis, sint ut vel velit veniam vero. Debitis.</p>
-                            <div class="col-md-offset-2">
-                                <p><strong>Horaire</strong></p>
-                                <p><strong>Lieu</strong></p>
-                                <p><strong>Information</strong></p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque aut dignissimos ea libero, magnam nemo placeat repellendus ut voluptatibus. Animi corporis distinctio et ipsum vero. Magnam quae sed sint. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores earum eius et fugit molestiae, necessitatibus nulla obcaecati officia placeat quae quas quis, sint ut vel velit veniam vero.</p>
+                            <div class="col-md-offset-2 information-1">
+                                <p>
+                                    <i class="fa fa-clock-o" aria-hidden="true"></i><strong> Horaire : </strong>9h-12h30
+                                </p>
+                                <p>
+                                    <i class="fa fa-map-marker" aria-hidden="true"></i><strong> Lieu : </strong>CCIP - 27 avenenu de Friedland - 75008
+                                </p>
+                                <p>
+                                    <i class="fa fa-info-circle" aria-hidden="true"></i><strong> Information : </strong>01 55 65 66 72 / 03 ou
+                                    <a href="#">www.dfc.ccip.fr</a>
+                                </p>
                             </div>
-                            <a href="">Se renseigner et choisir</a>
+                            <a href="">
+                                <i class="fa fa-caret-right" aria-hidden="true"></i> Se renseigner et choisir
+                            </a>
                         </div>
                         <div class="bourses">
                             <h2>Bourses</h2>
                             <h3>Les rendez-vous formation & compétences</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque aut dignissimos ea libero, magnam nemo placeat repellendus ut voluptatibus. Animi corporis distinctio et ipsum vero. Magnam quae sed sint.</p>
-                            <p><strong>Horaire</strong></p>
-                            <p><strong>Lieu</strong></p>
-                            <p><strong>Information</strong></p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque aut dignissimos ea libero, magnam nemo placeat repellendus ut voluptatibus. Animi corporis distinctio et ipsum vero. Magnam quae sed sint. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores earum eius et fugit molestiae, necessitatibus nulla obcaecati officia placeat quae quas quis, sint ut vel velit veniam vero.</p>
+                            <div class="information-2">
+                                <p>
+                                    <i class="fa fa-clock-o" aria-hidden="true"></i><strong> Horaire : </strong>9h-12h30
+                                </p>
+                                <p>
+                                    <i class="fa fa-map-marker" aria-hidden="true"></i><strong> Lieu : </strong>CCIP - 27 avenenu de Friedland - 75008
+                                </p>
+                                <p>
+                                    <i class="fa fa-info-circle" aria-hidden="true"></i><strong> Information : </strong>01 55 65 66 72 / 03 ou
+                                    <a href="#">www.dfc.ccip.fr</a>
+                                </p>
+                            </div>
                             <div class="button-bourses text-right">
-                                <a href="" class="btn btn-info">En savoir +</a>
+                                <a href="" class="boutton-savoir">En savoir +</a>
                             </div>
                         </div>
                         <div class="marches">
                             <h2>Marchés publics</h2>
-                            <h3>Les rendez-vous formation & compétences</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque aut dignissimos ea libero, magnam nemo placeat repellendus ut voluptatibus. Animi corporis distinctio et ipsum vero. Magnam quae sed sint.</p>
+                            <h3>Une offre de marché public est en cours actuellement :</h3>
+                            <p class="marches-p1">- Remplacement de l'alarme intrusion</p>
+                            <p class="marches-p2">Date limite de réception des offres mardi 5 avril 2016</p>
+                            <br>
+                            <p class="marches-p3">Cliquer sur le lien ci-dessous pour télécharger le cahier des charges : </p>
+                            <a href="#" class="marches-p4">mapa alarme intrusion</a>
+                            <br>
+                            <br>
+                            <p class="marches-p5">Attribution récente : </p>
+                            <p class="marches-p6">Remplacement d'une presse numérique par une neuve ou d'occasion :</p>
+                            <p class="marches-p7">XEICON France, 11-13 rue des hautes Pâtures, 92100 NANTERRE : 89 900 € HT</p>
+                            <div class="button-bourses text-right">
+                                <a href="" class="boutton-savoir">En savoir +</a>
+                            </div>
                         </div>
                     </div>
                     <div class="col-md-4 contenu-entreprise-2">
                         <div class="offres">
                             <h2>Offres d'emplois</h2>
-                            
+                            <div class="offres-arts-graphiques">
+                                <h4>Dans les arts graphiques</h4>
+                                <div class="list-group text-center contenu-offres-arts">
+                                    <a href="#" class="list-group-item">Offres d'emploi et de stage</a>
+                                    <a href="#" class="list-group-item">Offres d'emploi graphistes et infographistes</a>
+                                    <a href="#" class="list-group-item">Studio créa, web design, illustration, graphisme...</a>
+                                    <a href="#" class="list-group-item">Site emploi des arts graphiques</a>
+                                </div>
+                            </div>
+                            <div class="offres-pif">
+                                <h4>Dans les arts graphiques</h4>
+                                <div class="text-center contenu-pif">
+                                    <a href="#" class="list-group-item">Offres impression</a>
+                                    <a href="#" class="list-group-item">Offres finition</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
