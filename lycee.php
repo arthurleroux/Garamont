c456951e671a2e0ae087cbb017fbc984e092b499
Arthur Leroux <Arthur@MacBook-Pro-de-Arthur-2.local>
2016-04-25 14:15:20 +0200

    début page lycée

--- lycee.php
+++ lycee.php
@@ -1,12 +1,27 @@
 <?php require("header.php"); ?>
 
 <div class="lycee">
-    <div class="container-fluid img-section">
+    <div class="container-fluid img-section no-padding">
+        <img src="img/lycee.jpg" alt="">
         <div class="mask">
         </div>
     </div>
     <div class="container-fluid temoignage-section">
-
+        <div class="row">
+            <div class="col-md-2 col-md-offset-1">
+                <h2 class="garamond">temoignages :</h2>
+            </div>
+            <div class="col-md-6 text-center">
+               <div class="row">
+                   <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet cumque exercitationem natus suscipit? Ab ad alias dicta eos neque nisi officiis perspiciatis placeat quas? Impedit inventore quasi sint ut."</p>
+                   <p><b><i>Auteur</i></b></p>
+                   <button href="" class="btn btn-histoire">Histoire</button>
+               </div>
+            </div>
+            <div class="col-md-3 points-verts no-padding">
+                <img src="img/element_vert.png" alt="">
+            </div>
+        </div>
     </div>
 </div>
 
