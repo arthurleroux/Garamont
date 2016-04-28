e4ce20cfe4338c219240b4d369ea99a80e1d0057
Elies Makhlouf <elies.makhlouf@gmail.comb@domain.ltd>
2016-04-28 17:40:13 +0200

    Page projets : établissements des éléments

--- projets.php
+++ projets.php
@@ -1,13 +1,193 @@
 <?php require("header.php"); ?>
 
-    <div class="visuel-projets">
-        <div class="container-fluid img-section">
-            <div class="mask"></div>
-        </div>
+    <div class="container text-center projets-titres">
+        <h1>Projets</h1>
     </div>
 
-    <div class="container text-center">
-        <h1>Projets</h1>
+        <div class="container-fluid projets-tabs">
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="navbar-collapse text-center tab-1" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav" role="tablist">
+                    <li role="presentation" class="active">
+                        <a href="#category1" aria-controls="category1" role="tab" data-toggle="tab">Catégorie 1</a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#category2" aria-controls="category2" role="tab" data-toggle="tab">Catégorie 2</a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#category3" aria-controls="category3" role="tab" data-toggle="tab">Catégorie 3</a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#category4" aria-controls="category4" role="tab" data-toggle="tab">Catégorie 4</a>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+        <!-- Tab panes -->
+        <div class="tab-content projets-visuels">
+            <div role="tabpanel" class="tab-pane fade in active" id="category1">
+                <div class="row projets-visuels-1">
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask">
+                            <i class="fa fa-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                </div>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="category2">
+                <div class="row projets-visuels-2">
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                </div>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="category3">
+                <div class="row projets-visuels-3">
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                </div>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="category4">
+                <div class="row projets-visuels-4">
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="col-md-4 no-padding projets-items">
+                        <img class="img-responsive" src="img/formation.jpg" alt="">
+                        <div class="mask"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 <?php require("footer.php"); ?>
