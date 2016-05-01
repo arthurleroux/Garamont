003972ee4537dc55b172793b87a30e2414bb2886
Arthur Leroux <Arthur@MacBook-Pro-de-Arthur-2.local>
2016-05-01 17:12:44 +0200

    menu full responsive

--- lycee.php
+++ lycee.php
@@ -6,12 +6,13 @@
         <div class="mask">
         </div>
     </div>
+
     <div class="container-fluid temoignage-section">
         <div class="row">
-            <div class="col-md-2 col-md-offset-1">
+            <div class="col-md-3 col-sm-3 text-right">
                 <h2 class="garamond">temoignages :</h2>
             </div>
-            <div class="col-md-6 text-center">
+            <div class="col-md-6 col-sm-6 text-center">
                <div class="row">
                    <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet cumque exercitationem natus suscipit? Ab ad alias dicta eos neque nisi officiis perspiciatis placeat quas? Impedit inventore quasi sint ut."</p>
                    <p><b><i>Auteur</i></b></p>
@@ -23,6 +24,7 @@
             </div>
         </div>
     </div>
+
     <div class="container-fluid  paragraphes-section garamond">
         <div class="row bg-gris">
             <div class="col-md-5 col-md-offset-1 col-xs-10 col-xs-offset-1">
@@ -41,13 +43,21 @@
             </div>
         </div>
         <div class="row p2">
+            <div class="col-md-5 col-md-offset-0 visible-xs visible-sm col-xs-10 col-xs-offset-1">
+                <div class="row img-row">
+                    <img src="img/img_lycee.JPG" alt="">
+                </div>
+                <div class="row img-row">
+                    <img src="img/elements_3.png" alt="">
+                </div>
+            </div>
             <div class="col-md-5 col-md-offset-1 col-xs-10 col-xs-offset-1 padding-right">
                 <h3>Lorem</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto autem et eveniet ex exercitationem explicabo hic, illo iusto libero nesciunt, obcaecati officiis, optio perferendis quaerat quia quibusdam saepe sit tempora.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid, commodi iusto labore nemo nesciunt quasi reiciendis similique voluptates. Earum error id impedit laborum odit quia saepe. Illum, ipsam, nulla?</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi iusto omnis porro quis vel? Culpa ea exercitationem facilis nobis pariatur placeat provident? Assumenda atque commodi debitis incidunt voluptas. Placeat, velit!</p>
             </div>
-            <div class="col-md-5 col-md-offset-0 col-xs-10 col-xs-offset-1">
+            <div class="col-md-5 col-md-offset-0 hidden-xs hidden-sm col-xs-10 col-xs-offset-1">
                 <div class="row img-row">
                     <img src="img/img_lycee.JPG" alt="">
                 </div>
