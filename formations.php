301712fe862ba0f3406bc18f7e80e8814bd1e9a2
Arthur Leroux <Arthur@MacBook-Pro-de-Arthur-2.local>
2016-05-19 14:59:52 +0200

    début page formations

--- formations.php
+++ formations.php
@@ -1,14 +1,115 @@
 <?php require("header.php"); ?>
 
-    <div class="visuel-formation">
-        <div class="container-fluid img-section">
-            <div class="mask">
+<div class="formations garamond">
+    <div class="container-fluid img-section no-padding">
+        <img src="img/img_formations.jpg" alt="">
+    </div>
+    <div class="container-fluid no-padding">
+
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" role="tablist">
+           <div class="col-md-offset-1 col-md-2 text-center">
+               <li role="presentation" class="active">
+                   <a href="#toutes" aria-controls="toutes" role="tab" data-toggle="tab">
+                       <b class="toutes">Toutes les formations</b>
+                   </a>
+               </li>
+           </div>
+           <div class="col-md-2 text-center">
+               <li role="presentation">
+                   <a href="#ind_graph" aria-controls="ind_graph" role="tab" data-toggle="tab">
+                       <span class="vert">
+                           bac pro
+                       </span>
+                       <br>
+                       Industries graphiques
+                   </a>
+               </li>
+           </div>
+           <div class="col-md-2 text-center">
+               <li role="presentation">
+                   <a href="#com_grap" aria-controls="com_grap" role="tab" data-toggle="tab">
+                      <span class="vert">
+                           b.t.s
+                      </span>
+                       <br>
+                       Communication et industrie graphique
+                   </a>
+               </li>
+           </div>
+            <div class="col-md-2 text-center">
+                <li role="presentation">
+                    <a href="#artisanat" aria-controls="artisanat" role="tab" data-toggle="tab">
+                        <span class="vert">
+                            bac pro
+                        </span>
+                        <br>
+                        Artisanat des métiers d'art
+                    </a>
+                </li>
+            </div>
+            <div class="col-md-2 text-center">
+                <li role="presentation">
+                    <a href="#design" aria-controls="design" role="tab" data-toggle="tab">
+                        <span class="vert">
+                            bac pro
+                        </span>
+                        <br>
+                        Design graphique
+                    </a>
+                </li>
+            </div>
+        </ul>
+    </div>
+    <div class="container">
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="toutes">
+                <div class="row">
+                    <div class="col-md-5 col-md-offset-1">
+                        <img class="losange" src="img/ind_grap.png" alt="">
+                        <div class="row">
+                            <div class="item item-1">
+                            </div>
+                            <!--<div class="triangle">
+                            </div>-->
+                        </div>
+                        <div class="row">
+                            <div class="item item-2">
+                            </div>
+                           <!-- <div class="triangle">
+                            </div>-->
+                        </div>
+                        <div class="row">
+                            <div class="item item-3">
+                            </div>
+                            <!--<div class="triangle">
+                            </div>-->
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+
+                    </div>
+                </div>
+            </div>
+
+            <div role="tabpanel" class="tab-pane" id="ind_graph">
+                Industries graphiques
+            </div>
+
+            <div role="tabpanel" class="tab-pane" id="com_grap">
+                Communication et industrie graphique
+            </div>
+
+            <div role="tabpanel" class="tab-pane" id="artisanat">
+                Artisanat des métiers d'arts
+            </div>
+
+            <div role="tabpanel" class="tab-pane" id="design">
+                Design graphique
             </div>
         </div>
     </div>
-
-    <div class="container text-center">
-        <h1>Formations</h1>
-    </div>
+</div>
 
 <?php require("footer.php"); ?>
