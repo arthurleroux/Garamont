e97746624f2a20e3717437822e04a228a280fe22
Arthur Leroux <Arthur@MacBook-Pro-de-Arthur-2.local>
2016-05-24 21:59:29 +0200

    continuation page formation single

--- formations.php
+++ formations.php
@@ -7,7 +7,7 @@
         <div class="container-fluid no-padding">
 
             <!-- Nav tabs -->
-            <ul class="nav nav-tabs" role="tablist">
+            <ul class="nav nav-tabs nav-top" role="tablist">
 
                 <li role="presentation" class="active col-md-2 col-md-offset-1 text-center">
                     <a href="#toutes" aria-controls="toutes" role="tab" data-toggle="tab">
@@ -55,7 +55,7 @@
         </div>
             <!-- Tab panes -->
             <div class="tab-content">
-                <div role="tabpanel" class="tab-pane active" id="toutes">
+                <div role="tabpanel" class="tab-pane fade  in active" id="toutes">
                     <div class="container">
                         <div class="row">
                             <div class="col-md-5 col-md-offset-1">
@@ -125,26 +125,34 @@
                     </div>
                 </div>
 
-                <div role="tabpanel" class="tab-pane" id="ind_graph">
+                <div role="tabpanel" class="tab-pane fade" id="ind_graph">
                     <div class="container-fluid">
-                        <div class="col-md-3 title-div">
-                            <div class="col-md-4 text-center vert">
-                                <h4>Bac pro</h4>
+                        <div class="col-md-3">
+                            <div class="title-div">
+                                <div class="col-md-4 text-center vert">
+                                    <h4>Bac pro</h4>
+                                </div>
+                                <div class="col-md-8 text-center border-left">
+                                    <h4>Industrie graphique</h4>
+                                </div>
                             </div>
-                            <div class="col-md-8 text-center border-left">
-                                <h4>Industrie graphique</h4>
-                            </div>
+                            <ul class="nav-side col-md-offset-3" role="tablist">
+                                <li role="presentation" class="active"><a href="#ind" aria-controls="ind" role="tab" data-toggle="tab">Façonnage industriel et routage</a></li>
+                                <li role="presentation"><a href="#impr" aria-controls="impr" role="tab" data-toggle="tab">Production imprimée</a></li>
+                                <li role="presentation"><a href="#graph" aria-controls="graph" role="tab" data-toggle="tab">Production graphique</a></li>
+                            </ul>
                         </div>
                         <div class="col-md-7 text-div">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab blanditiis dolorem reprehenderit. Aspernatur culpa dolorum explicabo, fugit hic nihil omnis sed tempora. Eaque earum enim fugiat, fugit saepe sed tenetur.
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad animi aspernatur at autem consequatur dolore ea eius excepturi itaque libero quis quo reiciendis sunt tenetur ut, voluptates, voluptatum. Iusto.
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi at beatae commodi consectetur consequuntur deserunt error, exercitationem, in natus, nihil obcaecati officia optio provident sapiente similique suscipit tempora vero!
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
+                            <div class="tab-content">
+                                <div role="tabpanel" class="tab-pane fade in active" id="ind"><h2>Façonnage industriel et routage</h2></div>
+                                <div role="tabpanel" class="tab-pane fade " id="impr"><h2>Production imprimée</h2></div>
+                                <div role="tabpanel" class="tab-pane fade" id="graph"><h2>Production graphique</h2></div>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div role="tabpanel" class="tab-pane" id="com_grap">
+                <div role="tabpanel" class="tab-pane fade" id="com_grap">
                     <div class="container-fluid">
                         <div class="col-md-3 title-div">
                             <div class="col-md-4 text-center vert">
@@ -163,7 +171,7 @@
                     </div>
                 </div>
 
-                <div role="tabpanel" class="tab-pane" id="artisanat">
+                <div role="tabpanel" class="tab-pane fade" id="artisanat">
                     <div class="container-fluid">
                         <div class="col-md-3 title-div">
                             <div class="col-md-4 text-center vert">
@@ -182,7 +190,7 @@
                     </div>
                 </div>
 
-                <div role="tabpanel" class="tab-pane" id="design">
+                <div role="tabpanel" class="tab-pane fade" id="design">
                     <div class="container-fluid">
                         <div class="col-md-3 title-div">
                             <div class="col-md-4 text-center vert">
