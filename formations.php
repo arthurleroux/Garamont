78252aac57a444673eb8bb1d61151becb24f13c3
Arthur Leroux <Arthur@MacBook-Pro-de-Arthur-2.local>
2016-05-24 20:02:00 +0200

    continuation page formation single

--- formations.php
+++ formations.php
@@ -9,12 +9,12 @@
             <!-- Nav tabs -->
             <ul class="nav nav-tabs" role="tablist">
 
-                <li role="presentation" class="active col-md-2 col-md-offset-1">
+                <li role="presentation" class="active col-md-2 col-md-offset-1 text-center">
                     <a href="#toutes" aria-controls="toutes" role="tab" data-toggle="tab">
                         <b class="toutes">Toutes les formations</b>
                     </a>
                 </li>
-                <li role="presentation" class="col-md-2">
+                <li role="presentation" class="col-md-2 text-center">
                     <a href="#ind_graph" aria-controls="ind_graph" role="tab" data-toggle="tab">
                        <span class="vert">
                            bac pro
@@ -23,7 +23,7 @@
                         Industries graphiques
                     </a>
                 </li>
-                <li role="presentation" class="col-md-2">
+                <li role="presentation" class="col-md-2 text-center">
                     <a href="#com_grap" aria-controls="com_grap" role="tab" data-toggle="tab">
                       <span class="vert">
                            b.t.s
@@ -32,7 +32,7 @@
                         Communication et industrie graphique
                     </a>
                 </li>
-                <li role="presentation" class="col-md-2">
+                <li role="presentation" class="col-md-2 text-center">
                     <a href="#artisanat" aria-controls="artisanat" role="tab" data-toggle="tab">
                     <span class="vert">
                         bac pro
@@ -41,7 +41,7 @@
                         Artisanat des métiers d'art
                     </a>
                 </li>
-                <li role="presentation" class="col-md-2">
+                <li role="presentation" class="col-md-2 text-center">
                     <a href="#design" aria-controls="design" role="tab" data-toggle="tab">
                     <span class="vert">
                         bac pro
@@ -53,71 +53,72 @@
 
             </ul>
         </div>
-        <div class="container">
             <!-- Tab panes -->
             <div class="tab-content">
                 <div role="tabpanel" class="tab-pane active" id="toutes">
-                    <div class="row">
-                        <div class="col-md-5 col-md-offset-1">
-                            <a href=""><img class="losange" src="img/ind_grap.png" alt=""></a>
-                            <div class="row">
-                                <div class="item item-1 text-right padding-right">
-                                    <p>Façonnage industriel et routage</p>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-5 col-md-offset-1">
+                                <a href=""><img class="losange" src="img/ind_grap.png" alt=""></a>
+                                <div class="row">
+                                    <div class="item item-1 text-right padding-right-20">
+                                        <p>Façonnage industriel et routage</p>
+                                    </div>
+                                    <div class="triangle triangle-1">
+                                    </div>
                                 </div>
-                                <div class="triangle triangle-1">
+                                <div class="row">
+                                    <div class="item item-2 text-right padding-right-20">
+                                        <p>Production imprimée</p>
+                                    </div>
+                                    <div class="triangle triangle-2">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="item item-2 text-right padding-right">
-                                    <p>Production imprimée</p>
-                                </div>
-                                 <div class="triangle triangle-2">
-                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="item item-3 text-right padding-right">
-                                    <p>Production graphique</p>
-                                </div>
-                                <div class="triangle triangle-3">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-5">
-                            <a href=""><img class="losange" src="img/com_graph.png" alt=""></a>
-                            <div class="row">
-                                <div class="item item-4 text-right padding-right">
-                                    <p><span>Études et réalisation de <br> produits imprimés</span></p>
-                                </div>
-                                <div class="triangle triangle-4">
+                                <div class="row">
+                                    <div class="item item-3 text-right padding-right-20">
+                                        <p>Production graphique</p>
+                                    </div>
+                                    <div class="triangle triangle-3">
+                                    </div>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="item item-5 text-right padding-right">
-                                    <p><span>Études et réalisation de <br> produits graphiques</span></p>
+                            <div class="col-md-5">
+                                <a href=""><img class="losange" src="img/com_graph.png" alt=""></a>
+                                <div class="row">
+                                    <div class="item item-4 text-right padding-right-20">
+                                        <p><span>Études et réalisation de <br> produits imprimés</span></p>
+                                    </div>
+                                    <div class="triangle triangle-4">
+                                    </div>
                                 </div>
-                                <div class="triangle triangle-5">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row margin-top-20">
-                        <div class="col-md-5 col-md-offset-1">
-                            <a href=""><img class="losange" src="img/artisanat.png" alt=""></a>
-                            <div class="row">
-                                <div class="item item-2 text-right padding-right">
-                                    <p><span>Communication <br> visuelle</span></p>
-                                </div>
-                                <div class="triangle triangle-2">
+                                <div class="row">
+                                    <div class="item item-5 text-right padding-right-20">
+                                        <p><span>Études et réalisation de <br> produits graphiques</span></p>
+                                    </div>
+                                    <div class="triangle triangle-5">
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-5">
-                            <a href=""><img class="losange" src="img/design.png" alt=""></a>
-                            <div class="row">
-                                <div class="item item-2 text-right padding-right">
-                                    <p><span>Communication et <br> médias imprimés</span></p>
+                        <div class="row margin-top-20">
+                            <div class="col-md-5 col-md-offset-1">
+                                <a href=""><img class="losange" src="img/artisanat.png" alt=""></a>
+                                <div class="row">
+                                    <div class="item item-2 text-right padding-right-20">
+                                        <p><span>Communication <br> visuelle</span></p>
+                                    </div>
+                                    <div class="triangle triangle-2">
+                                    </div>
                                 </div>
-                                <div class="triangle triangle-2">
+                            </div>
+                            <div class="col-md-5">
+                                <a href=""><img class="losange" src="img/design.png" alt=""></a>
+                                <div class="row">
+                                    <div class="item item-2 text-right padding-right-20">
+                                        <p><span>Communication et <br> médias imprimés</span></p>
+                                    </div>
+                                    <div class="triangle triangle-2">
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -125,30 +126,79 @@
                 </div>
 
                 <div role="tabpanel" class="tab-pane" id="ind_graph">
-                    <div class="col-md-3">
-
-                    </div>
-                    <div class="col-md-8 col-md-offset-1">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
+                    <div class="container-fluid">
+                        <div class="col-md-3 title-div">
+                            <div class="col-md-4 text-center vert">
+                                <h4>Bac pro</h4>
+                            </div>
+                            <div class="col-md-8 text-center border-left">
+                                <h4>Industrie graphique</h4>
+                            </div>
+                        </div>
+                        <div class="col-md-7 text-div">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab blanditiis dolorem reprehenderit. Aspernatur culpa dolorum explicabo, fugit hic nihil omnis sed tempora. Eaque earum enim fugiat, fugit saepe sed tenetur.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad animi aspernatur at autem consequatur dolore ea eius excepturi itaque libero quis quo reiciendis sunt tenetur ut, voluptates, voluptatum. Iusto.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi at beatae commodi consectetur consequuntur deserunt error, exercitationem, in natus, nihil obcaecati officia optio provident sapiente similique suscipit tempora vero!
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
+                        </div>
                     </div>
                 </div>
 
                 <div role="tabpanel" class="tab-pane" id="com_grap">
-                    Communication et industrie graphique
+                    <div class="container-fluid">
+                        <div class="col-md-3 title-div">
+                            <div class="col-md-4 text-center vert">
+                                <h4>b.t.s</h4>
+                            </div>
+                            <div class="col-md-8 text-center border-left">
+                                <h4>Communication et industrie graphique</h4>
+                            </div>
+                        </div>
+                        <div class="col-md-7 text-div">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab blanditiis dolorem reprehenderit. Aspernatur culpa dolorum explicabo, fugit hic nihil omnis sed tempora. Eaque earum enim fugiat, fugit saepe sed tenetur.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad animi aspernatur at autem consequatur dolore ea eius excepturi itaque libero quis quo reiciendis sunt tenetur ut, voluptates, voluptatum. Iusto.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi at beatae commodi consectetur consequuntur deserunt error, exercitationem, in natus, nihil obcaecati officia optio provident sapiente similique suscipit tempora vero!
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
+                        </div>
+                    </div>
                 </div>
 
                 <div role="tabpanel" class="tab-pane" id="artisanat">
-                    Artisanat des métiers d'arts
+                    <div class="container-fluid">
+                        <div class="col-md-3 title-div">
+                            <div class="col-md-4 text-center vert">
+                                <h4>bac pro</h4>
+                            </div>
+                            <div class="col-md-8 text-center border-left">
+                                <h4>artisanat des métiers d'arts</h4>
+                            </div>
+                        </div>
+                        <div class="col-md-7 text-div">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab blanditiis dolorem reprehenderit. Aspernatur culpa dolorum explicabo, fugit hic nihil omnis sed tempora. Eaque earum enim fugiat, fugit saepe sed tenetur.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad animi aspernatur at autem consequatur dolore ea eius excepturi itaque libero quis quo reiciendis sunt tenetur ut, voluptates, voluptatum. Iusto.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi at beatae commodi consectetur consequuntur deserunt error, exercitationem, in natus, nihil obcaecati officia optio provident sapiente similique suscipit tempora vero!
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
+                        </div>
+                    </div>
                 </div>
 
                 <div role="tabpanel" class="tab-pane" id="design">
-                    Design graphique
+                    <div class="container-fluid">
+                        <div class="col-md-3 title-div">
+                            <div class="col-md-4 text-center vert">
+                                <h4>bac pro</h4>
+                            </div>
+                            <div class="col-md-8 text-center border-left">
+                                <h4>design graphique</h4>
+                            </div>
+                        </div>
+                        <div class="col-md-7 text-div">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab blanditiis dolorem reprehenderit. Aspernatur culpa dolorum explicabo, fugit hic nihil omnis sed tempora. Eaque earum enim fugiat, fugit saepe sed tenetur.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad animi aspernatur at autem consequatur dolore ea eius excepturi itaque libero quis quo reiciendis sunt tenetur ut, voluptates, voluptatum. Iusto.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi at beatae commodi consectetur consequuntur deserunt error, exercitationem, in natus, nihil obcaecati officia optio provident sapiente similique suscipit tempora vero!
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi, debitis dolorem ea fuga, incidunt laudantium magnam maxime minus nam necessitatibus obcaecati placeat quod similique sit totam vel veritatis voluptate!</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
